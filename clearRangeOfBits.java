aa80897ac41235e6bd7722bac5fd979f720849ab
Kapil Rai <116628083+Imkapil07@users.noreply.github.com>
2023-07-18 22:58:53 +0530

    Clear range of bits

---
+++ clearRangeOfBits.java
@@ -0,0 +1,11 @@
+public class clearRangeOfBits {
+   public static int ClearInRange(int n,int i,int j){
+    int a = ((~0)<<(j+1));
+    int b = (1<<i)-1;
+    int bitMask = a | b;
+    return n & bitMask;
+   } 
+   public static void main(String[] args) {
+    System.out.println(ClearInRange(10, 2, 4));
+   }
+}
