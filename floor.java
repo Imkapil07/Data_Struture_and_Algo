7e7d567b9ab382f06cf48b5d38ebea8b811b5543
Kapil Rai <116628083+Imkapil07@users.noreply.github.com>
2023-08-06 21:02:18 +0530

    flooring

---
+++ floor.java
@@ -0,0 +1,26 @@
+public class floor {
+    public static void main(String[] args) {
+        int []arr = {2,3,5,9,14,16,18};
+        int target = 13;
+        int ans = floorV(arr, target);
+        System.out.println(ans);   
+    } 
+
+    static int floorV(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return end;
+   }
+}
