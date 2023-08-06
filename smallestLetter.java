fdb0accc94646a3b25edc23dde6dde67f92692f2
Kapil Rai <116628083+Imkapil07@users.noreply.github.com>
2023-08-06 21:02:56 +0530

    Samllest letter greater then target

---
+++ smallestLetter.java
@@ -0,0 +1,24 @@
+public class smallestLetter {
+    public static void main(String[] args) {
+      char[] letters = {'c','f','j'};
+      char target = 'j';
+      char ans = nextGreatestLetter(letters, target);
+      System.out.println(ans);
+    }
+    public static char nextGreatestLetter(char[] letters, char target) {
+
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
