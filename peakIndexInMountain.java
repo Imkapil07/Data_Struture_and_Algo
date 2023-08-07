2ec7d91ace888dd6db2f5d16a3161f37de58b02e
Kapil Rai <116628083+Imkapil07@users.noreply.github.com>
2023-08-08 00:05:45 +0530

    Find peak index in mountain

---
+++ peakIndexInMountain.java
@@ -0,0 +1,24 @@
+public class peakIndexInMountain {
+    public static void main(String[] args) {
+     int[] arr = {1,2,3,5,7,6,3,2};
+     System.out.println(peakIndexInMountainArray(arr));
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                // you are in dec part of array
+                // this may be the ans, but look at left
+                // this is why end != mid - 1
+                end = mid;
+            } else {
+                // you are in asc part of array
+                start = mid + 1; // because we know that mid+1 element > mid element
+            }
+        }
+        return start; // or return end as both are =
+    }
+}
