de1cc294222bda117a0dcb4546b8645f68241f66
Kapil Rai <116628083+Imkapil07@users.noreply.github.com>
2023-08-17 23:52:47 +0530

    Add files via upload

---
+++ stringQue01.java
@@ -0,0 +1,7 @@
+// Use of (.) Mean any charater or symbol
+public class stringQue01 {
+    public static void main(String[] args) {
+        String str = "f";
+        System.out.println(str.matches("."));
+    }
+}
